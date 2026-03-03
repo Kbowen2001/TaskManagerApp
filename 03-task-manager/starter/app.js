5375cf0ff06023922e4ea1910f143ac224ceb12f
Kbowen2001 <Kiarrabowen2001@gmail.com>
2026-03-03 12:31:11 -0700

    ad

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -21,4 +21,4 @@ app.listen(port, () => {
 // app.post('/api/v1/tasks') -> create a new task
 // app.get('/api/v1/tasks/:id') -> get a single task
 // app.patch('/api/v1/tasks/:id') -> update a task
-// app.delete('/api/v1/tasks/:id') -> delete a task
+// app.delete('/api/v1/tasks/:id') -> delete a task 
