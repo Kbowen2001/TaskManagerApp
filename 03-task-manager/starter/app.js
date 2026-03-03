95cf5dd2b8ea11fbc1a93cad402398faa757be8c
Kbowen2001 <Kiarrabowen2001@gmail.com>
2026-03-03 12:29:48 -0700

    added notes to apps js
    EVERYTHING WORKS HERE

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -14,3 +14,11 @@ const port = 3000;
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
+
+
+
+// app.get('/api/v1/tasks')  -> get all the tasks
+// app.post('/api/v1/tasks') -> create a new task
+// app.get('/api/v1/tasks/:id') -> get a single task
+// app.patch('/api/v1/tasks/:id') -> update a task
+// app.delete('/api/v1/tasks/:id') -> delete a task
