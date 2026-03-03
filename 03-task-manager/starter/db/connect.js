3331be110bfe20fb792415d5bd74fde6647652b8
Kbowen2001 <Kiarrabowen2001@gmail.com>
2026-03-03 13:34:53 -0700

    edit

--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,1 +1,8 @@
+
+const mongoose = require('mongoose');
+
+
 const connnectionString = 'mongodb+srv://Kiki:Kb324239@nodeexpressprojects.lnllk8p.mongodb.net/?appName=NodeExpressProjects';
+
+
+mongoose.connect()
