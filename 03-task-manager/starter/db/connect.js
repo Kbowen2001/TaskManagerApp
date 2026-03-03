29d439ea7e189cc2116b0f5e2e405d5cf00b3f33
Kbowen2001 <Kiarrabowen2001@gmail.com>
2026-03-03 13:40:37 -0700

    ad

--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -5,4 +5,8 @@ const mongoose = require('mongoose');
 const connnectionString = 'mongodb+srv://Kiki:Kb324239@nodeexpressprojects.lnllk8p.mongodb.net/?appName=NodeExpressProjects';
 
 
-mongoose.connect()
+mongoose
+    .connect(connnectionString)
+    .then(() => console.log('Connected to DB...'))
+    .catch((err) => console.log(err))
+;
