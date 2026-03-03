30f00467cb9f6409d735aa62f715e544f4f25b43
Kbowen2001 <Kiarrabowen2001@gmail.com>
2026-03-03 12:39:48 -0700

    adding to app js

--- 03-task-manager/starter/routes/tasks.js
+++ 03-task-manager/starter/routes/tasks.js
@@ -1,1 +1,10 @@
 const express = require('express'); 
+const router = express.Router();
+
+
+
+router.route('/').get((req, res) => {
+    res.send('Get all the tasks');
+});
+
+module.exports = router;
