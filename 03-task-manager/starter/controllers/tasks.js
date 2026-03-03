0a7a8a04bc9b1a07867c2f4288104e3b67b19c20
Kbowen2001 <Kiarrabowen2001@gmail.com>
2026-03-03 12:56:43 -0700

    adding routes

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -5,9 +5,21 @@ const getAllTasks = (req, res) => {
 };
 
 
+const createTask = (req, res) => {
+    res.send('create a new task');
+}
 
+const getTask = (req, res) => {
+    res.send('get a single task');
+}   
 
+const updateTask = (req, res) => {
+    res.send('update a task');
+}   
 
+const deleteTask = (req, res) => {
+    res.send('delete a task');
+}
 
 
 
