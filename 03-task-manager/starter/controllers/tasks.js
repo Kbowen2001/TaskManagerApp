96a06830938ea5e372611982ebe7309e2b2b9afe
Kbowen2001 <Kiarrabowen2001@gmail.com>
2026-03-04 13:31:23 -0700

    bug fix for update task

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -41,15 +41,20 @@ const getTask = async (req, res) => {
 
 
 const updateTask = async (req, res) => {
+    const { id: taskId } = req.params;
     try {
-        const { id: taskId } = req.params;
-        
         const task = await Task.findOneAndUpdate({ _id: taskId }, req.body, { new: true, runValidators: true });
         if (!task) {
             return res.status(404).json({ msg: `No task with id: ${taskId}` });
         }   
-        res.status(200).json({ id:taskId,data:req.body });
+        res.status(200).json({ task });
     } catch (error) {
+        if (error.name === 'CastError') {
+            return res.status(404).json({ msg: `No task with id: ${taskId}` });
+        }
+        if (error.name === 'ValidationError') {
+            return res.status(400).json({ msg: error.message });
+        }
         res.status(500).json({ msg: error.message });
     }
 }   
