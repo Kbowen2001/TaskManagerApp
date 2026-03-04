619d2e405ce720a31f522608a918c698397defac
Kbowen2001 <Kiarrabowen2001@gmail.com>
2026-03-04 13:52:15 -0700

    ad

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -75,12 +75,29 @@ const deleteTask = async (req, res) => {
 }
 
 
+const editTask = async (req, res) => {
+    try {
+        const { id: taskId } = req.params;
+        const task = await Task.findOneAndUpdate({ _id: taskId }, req.body, {
+             new: true, 
+             runValidators: true, 
+            overwrite: true
+            });
+        if (!task) {
+            return res.status(404).json({ msg: `No task with id: ${taskId}` });
+        }
+        res.status(200).json({ task });
+    } catch (error) {
+        res.status(500).json({ msg: error.message });
+    }
+}
 
 module.exports = {
     getAllTasks,
     createTask,
     getTask,
     updateTask,
-    deleteTask
+    deleteTask,
+    editTask
 }
 
