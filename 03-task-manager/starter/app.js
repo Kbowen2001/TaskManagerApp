fc6d0f994bf2f7a737eca1351495f7c1632d1856
Kbowen2001 <Kiarrabowen2001@gmail.com>
2026-03-04 10:10:50 -0700

    continue working after hour mark on video

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,9 +1,9 @@
 
-require('./db/connect');
+
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
-
+const connectDB = require('./db/connect');
 
 
 
@@ -23,6 +23,19 @@ const port = 3000;
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
+
+
+const start = async () => {
+    try {
+        await connectDB('mongodb://Kiki:Kb324239@ac-vdfhxqj-shard-00-00.lnllk8p.mongodb.net:27017,ac-vdfhxqj-shard-00-01.lnllk8p.mongodb.net:27017,ac-vdfhxqj-shard-00-02.lnllk8p.mongodb.net:27017/?authSource=admin&replicaSet=atlas-i0koh8-shard-0&tls=true&appName=NodeExpressProjects');
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 
 
 
--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const connnectionString =
   'mongodb://Kiki:Kb324239@ac-vdfhxqj-shard-00-00.lnllk8p.mongodb.net:27017,ac-vdfhxqj-shard-00-01.lnllk8p.mongodb.net:27017,ac-vdfhxqj-shard-00-02.lnllk8p.mongodb.net:27017/?authSource=admin&replicaSet=atlas-i0koh8-shard-0&tls=true&appName=NodeExpressProjects';
 
-mongoose
-  .connect(connnectionString)
-  .then(() => console.log('Connected to DB...'))
-  .catch((err) => console.log(err));
+const connectDB = (url) => {
+  return mongoose.connect(url);
+}
+
+
+module.exports = connectDB;
