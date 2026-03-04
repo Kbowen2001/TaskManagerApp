d833382940643c478ae382a8c945f39482d1a36d
Kbowen2001 <Kiarrabowen2001@gmail.com>
2026-03-04 10:17:11 -0700

    ad

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -4,17 +4,19 @@ const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
-
+require('dotenv').config();
 
 
 //middleware
-
 app.use(express.json());
+
 
 //routes
 app.get('/hello', (req, res) => {
     res.send('Task Manager App');
 });
+
+
 
 app.use('/api/v1/tasks', tasks);  
 
--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-const connnectionString =
-  'mongodb://Kiki:Kb324239@ac-vdfhxqj-shard-00-00.lnllk8p.mongodb.net:27017,ac-vdfhxqj-shard-00-01.lnllk8p.mongodb.net:27017,ac-vdfhxqj-shard-00-02.lnllk8p.mongodb.net:27017/?authSource=admin&replicaSet=atlas-i0koh8-shard-0&tls=true&appName=NodeExpressProjects';
+
 
 const connectDB = (url) => {
   return mongoose.connect(url);
