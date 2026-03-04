f9693b567b5922c05ae63a7d3b7a2b3367e05e7a
Kbowen2001 <Kiarrabowen2001@gmail.com>
2026-03-04 14:24:05 -0700

    asyn wrapper

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,9 +1,9 @@
 
 const Task = require('../models/task');
+const asyncWrapper = require('../middleware/async');
 
 
-
-const getAllTasks = async (req, res) => {
+const getAllTasks = asyncWrapper( async (req, res) => {
     try{
         const tasks = await Task.find({});
        res.status(200).json({ tasks });
@@ -11,7 +11,7 @@ const getAllTasks = async (req, res) => {
     catch(error){
         res.status(500).json({ msg: error.message });
     }
-};
+});
 
 
 const createTask = async (req, res) => {
