2fbbff19678dfb00fb09bac7914234d3e48c485d
Kbowen2001 <Kiarrabowen2001@gmail.com>
2026-03-04 14:25:18 -0700

    ad

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -4,13 +4,9 @@ const asyncWrapper = require('../middleware/async');
 
 
 const getAllTasks = asyncWrapper( async (req, res) => {
-    try{
+    
         const tasks = await Task.find({});
        res.status(200).json({ tasks });
-    }
-    catch(error){
-        res.status(500).json({ msg: error.message });
-    }
 });
 
 
