8ff7aab2b807e88f388877354d0548ddc51150bc
Kbowen2001 <Kiarrabowen2001@gmail.com>
2026-03-04 14:31:18 -0700

    fix shitstorm

--- 03-task-manager/starter/routes/tasks.js
+++ 03-task-manager/starter/routes/tasks.js
@@ -1,7 +1,7 @@
 const express = require('express'); 
 const router = express.Router();
 
-const {getAllTasks,createTask,getTask,updateTask,deleteTask,editTask} = require('../controllers/tasks');
+const {getAllTasks,createTask,getTask,updateTask,deleteTask} = require('../controllers/tasks');
 
 
 
