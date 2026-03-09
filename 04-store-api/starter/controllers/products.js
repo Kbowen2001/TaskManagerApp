3dc6c432a6fc4be03206ffb320d5bacf579f4918
Kbowen2001 <Kiarrabowen2001@gmail.com>
2026-03-09 14:19:43 -0600

    ad

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -10,7 +10,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
- 
+ const products = await Product.find(req.query);
   res.status(200).json({ products, nbHits: products.length });
 }
 
