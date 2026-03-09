4668a32101c218524c35d6bd01347bee05ac4abc
Kbowen2001 <Kiarrabowen2001@gmail.com>
2026-03-09 10:44:29 -0600

    ad

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -16,3 +16,24 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
 });
+
+
+//products route
+
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 3000;
+
+
+const start = async () => {
+    try {
+        //connectDB
+        app.listen(port, () => console.log(`Server is running on port ${port}`));
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
