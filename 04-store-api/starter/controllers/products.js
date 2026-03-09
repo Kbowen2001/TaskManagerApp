38e249890230b430217e5fda488a41a032a6acf3
Kbowen2001 <Kiarrabowen2001@gmail.com>
2026-03-09 13:50:42 -0600

    fuck

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,16 +1,18 @@
 
+const Product = require('../models/product');
+
 
 const getAllProductsStatic = async (req, res) => {
-    throw new Error('Testing async errors');
-    res.status(200).json({ msg: 'products testing route' });
-}
+  const products = await Product.find({});
+  res.status(200).json({ products, nbHits: products.length });
+};
 
 const getAllProducts = async (req, res) => {
-    res.status(200).json({ msg: 'products route' });
+  
+  res.status(200).json({ products, nbHits: products.length });
 }
 
 
 module.exports = {
-    getAllProducts,
-    getAllProductsStatic
+    getAllProducts, getAllProductsStatic,
 }
