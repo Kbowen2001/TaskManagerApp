548fd2f77144fa781e58cf6fcf24124e42ffc184
Kbowen2001 <Kiarrabowen2001@gmail.com>
2026-03-10 10:47:52 -0600

    fields add

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -9,7 +9,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -32,6 +32,11 @@ const getAllProducts = async (req, res) => {
   }
   else{
     result = result.sort('createdAt');
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ');
+    result = result.select(fieldsList);
   }
 
   const products = await result;
