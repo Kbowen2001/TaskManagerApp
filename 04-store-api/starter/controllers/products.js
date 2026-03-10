362d76a69ff4573bafef0565cc6b8de42b7f753b
Kbowen2001 <Kiarrabowen2001@gmail.com>
2026-03-10 10:56:36 -0600

    limit

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -4,7 +4,10 @@ const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
   
-  const products = await Product.find({}).select('name price');
+  const products = await Product.find({})
+  .sort('name')
+  .select('name price')
+  .limit(4);
   res.status(200).json({ products, nbHits: products.length });
 };
 
